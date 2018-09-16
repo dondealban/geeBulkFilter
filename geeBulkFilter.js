f854131d8e9b969e0936952b1e48ad09f4c9b1c2
Atreya Shankar <35427332+AtreyaSh@users.noreply.github.com>
2018-09-16 18:01:03 +0200

    indentation beautification

--- geeBulkFilter.js
+++ geeBulkFilter.js
@@ -10,18 +10,18 @@ var image3 = ee.Image("USGS/SRTMGL1_003");
 
 // Functions to calculate hillshade
 function radians(img) {
-return img.toFloat().multiply(Math.PI).divide(180);
+  return img.toFloat().multiply(Math.PI).divide(180);
 }
 
 // Functions to calculate hillshade
 function hillshade(az, ze, slope, aspect) {
-var azimuth = radians(ee.Image(az));
-var zenith = radians(ee.Image(ze));
-return azimuth.subtract(aspect).cos()
-.multiply(slope.sin())
-.multiply(zenith.sin())
-.add(
-zenith.cos().multiply(slope.cos()));
+  var azimuth = radians(ee.Image(az));
+  var zenith = radians(ee.Image(ze));
+  return azimuth.subtract(aspect).cos()
+    .multiply(slope.sin())
+    .multiply(zenith.sin())
+    .add(
+        zenith.cos().multiply(slope.cos()));
 }
 
 // Filtering Landast 8 SR ImageCollection
@@ -31,57 +31,57 @@ var collection = ee.ImageCollection('LANDSAT/LC8_SR') // query Landsat 8 SR Imag
 .filterDate('2011-01-01', '2017-05-01') // temporal filter
 .filter(ee.Filter.lte('CLOUD_COVER', 10)) // mean cloud score filter
 .map(function(image) {
-return image.clipToCollection(table2); // clip image
+  return image.clipToCollection(table2); // clip image
 })
 .map(function(image){ // return clear image without clouds, cloud-shadows, water and snow
-var clear = image.select('cfmask').eq(0); 
-clear = clear.updateMask(clear);
-return image.updateMask(clear);
+  var clear = image.select('cfmask').eq(0); 
+  clear = clear.updateMask(clear);
+  return image.updateMask(clear);
 })
 .map(function(image){ // remove pixels significantly affected by terrain-related shadows
-var imageClipped = image3.clip(table);
-var azimuthImage = image.metadata('solar_azimuth_angle');
-var zenithImage = image.metadata('solar_zenith_angle');
-var terrain = ee.Algorithms.Terrain(imageClipped);
-var slope = radians(terrain.select('slope'));
-var aspect = radians(terrain.select('aspect'));
-var hillshadeExp = hillshade(azimuthImage, zenithImage, slope, aspect);
-var hillshadeExpFilter = hillshadeExp.select(0).gt(0.01);
-hillshadeExpFilter = hillshadeExpFilter.updateMask(hillshadeExpFilter);
-var noList = ee.List([]);
-var float = image.projection().transform().split(', ');
-for(var i = 6; i <= 12; i += 2){
-var no = ee.Number.parse(ee.String(float.get(i)).replace(']', '').replace(']', ''));
-no = no.toFloat();
-noList = noList.add(no);
-}
-var noList2 = noList.insert(1, 0).insert(3, 0);
-var hillshadeExpFilterRP = hillshadeExpFilter.reproject('EPSG:32643', noList2);
-return image.updateMask(hillshadeExpFilterRP);
+  var imageClipped = image3.clip(table);
+  var azimuthImage = image.metadata('solar_azimuth_angle');
+  var zenithImage = image.metadata('solar_zenith_angle');
+  var terrain = ee.Algorithms.Terrain(imageClipped);
+  var slope = radians(terrain.select('slope'));
+  var aspect = radians(terrain.select('aspect'));
+  var hillshadeExp = hillshade(azimuthImage, zenithImage, slope, aspect);
+  var hillshadeExpFilter = hillshadeExp.select(0).gt(0.01);
+  hillshadeExpFilter = hillshadeExpFilter.updateMask(hillshadeExpFilter);
+  var noList = ee.List([]);
+  var float = image.projection().transform().split(', ');
+  for(var i = 6; i <= 12; i += 2){
+    var no = ee.Number.parse(ee.String(float.get(i)).replace(']', '').replace(']', ''));
+    no = no.toFloat();
+    noList = noList.add(no);
+  }
+  var noList2 = noList.insert(1, 0).insert(3, 0);
+  var hillshadeExpFilterRP = hillshadeExpFilter.reproject('EPSG:32643', noList2);
+  return image.updateMask(hillshadeExpFilterRP);
 })
 .select(['B1', 'B2', 'B3', 'B4', 'B5', 'B6', 'B7']); // export bands 1-7 relevant for analysis
 
 // Function/command to bulk export filtered ImageCollection
 var ExportCol = function(col, folder, scale,
-nimg, maxPixels, region) {
-nimg = nimg || 31;
-scale = scale || 30;
-maxPixels = maxPixels || 1e10;
-var colList = col.toList(nimg);
-var n = colList.size().getInfo();
-for (var i = 0; i < n; i++) {
-var img = ee.Image(colList.get(i));
-var id = img.id().getInfo();
-region = table2;
-Export.image.toDrive({
-image:img,
-description: id + '_' + i,
-folder: folder,
-region: region,
-scale: scale,
-maxPixels: maxPixels,
-});
-}
+    nimg, maxPixels, region) {
+  nimg = nimg || 31;
+  scale = scale || 30;
+  maxPixels = maxPixels || 1e10;
+  var colList = col.toList(nimg);
+  var n = colList.size().getInfo();
+  for (var i = 0; i < n; i++) {
+    var img = ee.Image(colList.get(i));
+    var id = img.id().getInfo();
+    region = table2;
+    Export.image.toDrive({
+      image:img,
+      description: id + '_' + i,
+      folder: folder,
+      region: region,
+      scale: scale,
+      maxPixels: maxPixels,
+    });
+  }
 };
 
 ExportCol(collection, "Landsat_Export", 30);
--- projection_stringComparison.js
+++ projection_stringComparison.js
@@ -10,12 +10,12 @@ var collection = ee.ImageCollection('LANDSAT/LC8_SR')
 .filterDate('2011-01-01', '2017-05-01')
 .filter(ee.Filter.lte('CLOUD_COVER', 10))
 .map(function(image) {
-return image.clipToCollection(table2);
+  return image.clipToCollection(table2);
 })
 .map(function(image){
-var clear = image.select('cfmask').eq(0);
-clear = clear.updateMask(clear);
-return image.updateMask(clear);
+  var clear = image.select('cfmask').eq(0);
+  clear = clear.updateMask(clear);
+  return image.updateMask(clear);
 });
 
 var trl = ee.List([[]]);
@@ -24,36 +24,36 @@ var trl2 = ee.List([[]]);
 // First method, the more appropriate but limited version
 
 function transformList(col) {
-    var nimg = 31;
-    var colList = col.toList(nimg);
-    
-    for (var i = 0; i < nimg; i++) {
-      var img = ee.Image(colList.get(i));
-      var tr = img.projection().getInfo().transform;
-      trl = trl.add(tr);
-    }
-    print(trl);
+  var nimg = 31;
+  var colList = col.toList(nimg);
+
+  for (var i = 0; i < nimg; i++) {
+    var img = ee.Image(colList.get(i));
+    var tr = img.projection().getInfo().transform;
+    trl = trl.add(tr);
   }
-  
+  print(trl);
+}
+
 // Second method, string-ly improvised
 
 function transformList2(col){
-   var nimg = 31;
-   var colList = col.toList(nimg);
+  var nimg = 31;
+  var colList = col.toList(nimg);
 
-    for (var i = 0; i < nimg; i++) {
+  for (var i = 0; i < nimg; i++) {
     var img = ee.Image(colList.get(i));
     var float = img.projection().transform().split(', ');
     var noList = ee.List([]);
     var noList2 = ee.List([]);
-    
+
     for(var j = 6; j <= 12; j += 2){
-    var no = ee.Number.parse(ee.String(float.get(j)).replace(']', '').replace(']', '')).toFloat();
-    noList = noList.add(no);
-       }
-    trl2 = trl2.add(noList.insert(1, 0).insert(3, 0));
+      var no = ee.Number.parse(ee.String(float.get(j)).replace(']', '').replace(']', '')).toFloat();
+      noList = noList.add(no);
     }
-    print(trl2);
+    trl2 = trl2.add(noList.insert(1, 0).insert(3, 0));
+  }
+  print(trl2);
 }
 
 transformList(collection);
